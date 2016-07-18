9408e3a2bb1ff4c2fdec5b0c9059820aa7a3ec0e
Yuqi He <yuqi.he@gmail.com>
2016-07-18 15:06:05 +0800

    Revert "Fixes the issue to display free edge."
    This reverts commit 08a5e9464390604f672738a05ecefc376cfc01a3.

--- src/OpenGl/OpenGl_PrimitiveArray.cxx
+++ src/OpenGl/OpenGl_PrimitiveArray.cxx
@@ -1406,7 +1406,6 @@ void OpenGl_PrimitiveArray::DrawDegeneratesAsLines (const TEL_COLOUR*           
       {
         for (i = n = 0; i < myPArray->num_bounds; ++i)
         {
-          if (pfc != NULL) glColor3fv (pfc[i].rgb);
           glDrawArrays (myDrawMode, n, myPArray->bounds[i]);
           n += myPArray->bounds[i];
         }
